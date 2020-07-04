70ae6de9f143892beb85bd39d3d33294dcb7c961
tedkkraft <tedkkraft@gmail.com>
2020-07-04 13:01:45 -0400

    On-click for search button built. Aligning response data to page divs WIP

--- javascript.js
+++ javascript.js
@@ -1,10 +1,40 @@
 var APIKey = "ca39045c73add718cdee665b1e7622de";
 
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + APIKey;
+var cityName = "austin"
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
 
 $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+}).then(function(response) {
+    var temp = resopnse.main.temp
+});
+
+var cities = [];
+
+function renderForecast() {
+    $("#searchHistory").empty();
+
+   // for (var i = 0; i < cities.length; i++) {
+      //  var a = 
+
+    //}
+    
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+}
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var citySearchVal = $("#citySearch").val().trim();
+    cities.push(citySearchVal);
+    console.log(cities);
+    renderForecast();
+});
