6cb21aefe2b866b9e302e35dc7c9a11102b32ec6
tedkkraft <tedkkraft@gmail.com>
2020-07-04 21:10:04 -0400

    Roadblock trying to create 5-day forecast. Broken for loop.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # weather-dashboard
 A weather dashboard that runs in the browser and uses the OpenWeather API to retrieve weather data for cities. 
+
+Enter a city into the searchbar to get the city's current weather and *5-day forecast.
+
+***This application is not fully functional. I ran into a roadblock creating the 5 day forecast.
+
+
--- javascript.js
+++ javascript.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
 
     function renderCurrent(searchCity) {
-        console.log("inside renderForecast");
-        
+
         var APIKey = "ca39045c73add718cdee665b1e7622de";
 
         var currentURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIKey;
-
-        var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=" + APIKey;
 
         $.ajax({
             url: currentURL,
@@ -37,17 +34,33 @@ $(document).ready(function(){
         
         .then(function(response) {
             console.log(response);
-            var forecastCard = $("<card class='forecast'>");
-            var ufDate = response.list[0].dt
-            console.log(ufDate);
-            var date = ufDate;
+
+            // I'm struggling to make my 5 day forecast appear.
+            // The Call 16 day forecast requires a paid subscription,
+            // and the 5 day forecast gives data for every 3 hours (which I'm using here).
+            // This is why I am using "i + 8" to advance my loop 24 hours and loop 5 times (days) between i = 0 through 32 (i < 33).
+            // ** BUT the issue seems to be it's endlessly looping--the progress wheel continues to spin and nothing happens.
+
+            for (i = 0; i < 33; i + 8) {
+                var a = $("<card class='forecastCard'>");
+                $(a).append("<ol class='forecastItems'>");
+                var unformattedDate = response.list[i].dt;
+                var fiveDayTemp = (response.list[i].main.temp - 273.15) * 1.80 + 32;
+                var fiveDayHumidity = response.list[i].main.humidity;
+                $(".forecastItems").append("<li>" + unformattedDate + "</li>");
+                $(".forecastItems").append("<li>Temp: " + fiveDayTemp + " F</li>");
+                $(".forecastItems").append("<li>Humidity: " + fiveDayHumidity + "</li>");
+                $("#fiveDayForecast").append(a);
+            }
+  
+            
         })
     }
 
     $("#searchBtn").on("click", function(event) {
         event.preventDefault();
         var searchCity = $("#citySearch").val().trim();
-        // renderCurrent(searchCity);
+        renderCurrent(searchCity);
         renderForecast(searchCity);
     });
 
